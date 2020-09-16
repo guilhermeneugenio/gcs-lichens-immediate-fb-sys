42936af506dea9438801431aba3bb2f8a1979218
Guilherme Eugénio <guilhermeneugenio@hotmail.com>
2020-09-16 10:36:02 +0100

    DB Config Fix

--- server/extension/config.js
+++ server/extension/config.js
@@ -8,8 +8,8 @@ const admin = [
 
 // Database credentials
 const db = {
-    url: 'mongodb://127.0.0.1:27017/crowdsourcing',
-    name: 'crowdsourcing'
+    url: 'mongodb://127.0.0.1:27017/GCS-LIF',
+    name: 'GCS-LIF'
 };
 
 
