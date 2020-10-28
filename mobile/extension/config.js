df69b65634df178b6b7fa62dd0c874f4f6fbf2e4
Guilherme Eugénio <guilhermeneugenio@hotmail.com>
2020-10-28 18:58:54 +0000

    config Presentation

--- mobile/extension/config.js
+++ mobile/extension/config.js
@@ -1,4 +1,8 @@
+import presentation1 from "./presentation/presentation1";
+import presentation2 from "./presentation/presentation2";
+import presentation3 from "./presentation/presentation3";
 const config = {
+    presentation: [presentation1, presentation2, presentation3],
     credentials: {
         google: {
             webClientId: "608098992888-pk0vantp01d0avqh423djgodbpm1k86m.apps.googleusercontent.com",
@@ -10,9 +14,10 @@ const config = {
         }
     },
     //serverURL: 'http://146.193.41.162/lif/server',
-    serverURL: 'http://192.168.1.69:3000',
+    serverURL: 'http://192.168.1.69:3001',
     //Idioms of the app
-    languages:['en']
+    languages:['pt','en'],
+    ranking: 3
 };
 
 export default config;
